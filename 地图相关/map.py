e73d74c884777321e0f821bccbc416f62895aafd
jisifan <503623869@qq.com>
2018-03-03 21:05:17 +0800

    done

--- 地图相关/map.py
+++ 地图相关/map.py
@@ -1,7 +1,18 @@
 import os
 from math import radians, cos, sin, asin, sqrt
+import csv
+
+#容忍的最小距离
+tolerance = 2000
+
+#文件保存地址
+fileSave = 'C:\Temp\map\jieguo.csv'
 
 class LeastCover:
+	"""
+	最小覆盖矩形
+	"""
+
 	# def __init__(self,up,down,left,right):
 	# 	self.up = up
 	# 	self.down = down
@@ -17,11 +28,13 @@ class LeastCover:
 		
 	def toString(self):
 		print("up:"+ str(self.up) + "	down:" + str(self.down) + "	left:" + str(self.left) + "	right:" + str(self.right))
-	
-
 
 
 class JingWeiDu:
+	"""
+	两个List分别放经度和维度
+	"""
+
 	def __init__(self):
 		self.jingduList = list()
 		self.weiduList = list()
@@ -31,13 +44,15 @@ class JingWeiDu:
 
 
 class Measure:
-	"""经纬度的处理"""
+	"""
+	经纬度的处理
+	"""
 
-	def haversine(self,lon1, lat1, lon2, lat2): # 经度1，纬度1，经度2，纬度2 （十进制度数）  
+	def haversine(self,lon1, lat1, lon2, lat2): 
 		""" 
-		Calculate the great circle distance between two points  
-		on the earth (specified in decimal degrees) 
+		经度1，纬度1，经度2，纬度2 （十进制度数）  
 		"""  
+
 		# 将十进制度数转化为弧度  
 		lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
 
@@ -51,7 +66,7 @@ class Measure:
 
 	def isShortEnough(self,zoneA,zoneB):
 		""" 
-		两矩形的最短距离是否大致小于两公里，是返回true，否返回false
+		两矩形的最短距离是否大致小于tolerance，是返回true，否返回false
 		"""  
 
 		#纬度越高，经度单位差值越短
@@ -60,22 +75,21 @@ class Measure:
 
 		#扩张区域A，直至上下左右都移出至少两公里
 		leftC = zoneA.left
-		while self.haversine(leftC,stdweidu,zoneA.left,stdweidu) < 2000:
+		while self.haversine(leftC,stdweidu,zoneA.left,stdweidu) < tolerance:
 			leftC = leftC - 0.01
 
 		rightC = zoneA.right
-		while self.haversine(rightC,stdweidu,zoneA.right,stdweidu) < 2000:
+		while self.haversine(rightC,stdweidu,zoneA.right,stdweidu) < tolerance:
 			rightC = rightC + 0.01
 
 
 		upC = zoneA.up
-		while self.haversine(zoneA.left,upC,zoneA.left,zoneA.up) < 2000:
+		while self.haversine(zoneA.left,upC,zoneA.left,zoneA.up) < tolerance:
 			upC = upC + 0.01
 
 		downC = zoneA.down
-		while self.haversine(zoneA.left,downC,zoneA.left,zoneA.down) < 2000:
+		while self.haversine(zoneA.left,downC,zoneA.left,zoneA.down) < tolerance:
 			downC = downC - 0.01
-
 
 		# A的扩展矩形和B是否相交，如果相交返回true
 		if downC > zoneB.up or leftC > zoneB.right or zoneB.down > upC or zoneB.left > rightC:
@@ -84,22 +98,46 @@ class Measure:
 			return True
 
 
+	def shortestDis(self,setA,setB):
+		""" 
+		返回两个集合的最短距离
+		"""  
+		shortest = 100000
+		lenA = len(setA.jingduList)
+		lenB = len(setB.jingduList)
+		for i in range(lenA):
+			for j in range(lenB):
+				# print(str(setA.jingduList[i])+ ',' + str(setA.weiduList[i]) + ',' + str(setB.jingduList[j]) + ',' + str(setB.weiduList[j]))
+				temp = self.haversine(setA.jingduList[i],setA.weiduList[i],setB.jingduList[j],setB.weiduList[j])
+				# print(temp)
+				if temp < shortest:
+					shortest = temp
+		return shortest
+
+
 
 
 class FileHandle(object):
-	"""处理目录以及kml文件相关操作"""
+	"""
+	处理目录以及kml文件相关操作
+	"""
 
-	# 初始化时输入文件目录
-	def __init__(self,file_dir):   
+	def __init__(self,file_dir):  
+		"""
+		初始化时输入文件目录 
+		"""
 		for root, dirs, files in os.walk(file_dir):  
 			self.root = root
 			self.files = files
 			# 2表示未定义，1表示相邻，0表示不相邻
-			self.matrix = [[3 for i in range(len(files))] for i in range(len(files))]
+			self.matrix = [[2 for i in range(len(files))] for i in range(len(files))]
+			for i in range(len(files)):
+				self.matrix[i][i] = 1 
 
-
-	# 处理kml文件，获取所有经纬度对
 	def kmlReader(self,filename):
+		"""
+		处理kml文件，获取所有经纬度对
+		"""
 		with open(filename,'r',encoding = 'UTF-8') as f:
 			k = f.read()
 			templist = k.split("<coordinates>")
@@ -124,18 +162,25 @@ class FileHandle(object):
 		return([jingweidu,leastCover])
 
 
-	# 获取所有县的上下左右边界
 	def getBoundList(self):
+		"""
+		获取所有县的上下左右边界
+		"""
 		self.boundList = list()
+		self.areaList = list()
 		for file in self.files:
 			filename = self.root + '\\' + file
 			tt = self.kmlReader(filename)
 			self.boundList.append(tt[1])
+			self.areaList.append(tt[0])
 		# for item in boundList:
 		# 	item.toString()
 
 
 	def initialMatrix(self):
+		"""
+		初始化矩阵，第一个被调用
+		"""
 		me = Measure()
 		self.getBoundList()
 		for i in range(len(self.boundList)):
@@ -144,21 +189,82 @@ class FileHandle(object):
 					self.matrix[i][j] = 0
 					self.matrix[j][i] = 0
 
+	def ultimateProcessor(self):
+		"""
+		计算最终矩阵
+		"""
+		me = Measure()
+		for i in range(len(self.areaList)):
+			for j in range(i):
+				#如果不是距离特别远则进入这步计算
+				if self.matrix[i][j] == 2:
+					#比较这两个集合中的所有点之间的距离，取最小的
+					shortest = me.shortestDis(self.areaList[i],self.areaList[j])
+					print(shortest)
+					#如果两个集合之间的最短距离小于容忍度，则记为两县交界，否则记为不交界
+					if shortest < tolerance:
+						self.matrix[i][j] = 1
+						self.matrix[j][i] = 1
+					else:
+						self.matrix[i][j] = 0
+						self.matrix[j][i] = 0
+
+	#todo 打印成csv
+	def printCsv(self):
+		"""
+		打印最终矩阵
+		"""
+		with open(fileSave,'w') as f:
+			#写第一行
+			f.write('')
+			for i in range(len(self.files)):
+				f.write(',' + self.files[i].split(".kml")[0])
+			f.write('\n')
+			#写之后所有行
+			for i in range(len(self.files)):
+				f.write(self.files[i].split(".kml")[0])
+				for j in range(len(self.files)):
+					f.write(',' + str(self.matrix[i][j]))
+				f.write('\n')
 
 
+		
+
+
+
+
+
+me = Measure()
 k = FileHandle('C:\maps')
 k.initialMatrix()
-for i in range(len(k.matrix)):
-	for j in range(len(k.matrix)):
-		print(k.matrix[i][j])
-	print('\n')
+k.ultimateProcessor()
+k.printCsv()
+
+# print(k.files[1])
+# print(k.areaList[1].jingduList)
+# print(k.areaList[1].weiduList)
+
+
+# print(k.files[2])
+# print(k.areaList[2].jingduList)
+# print(k.areaList[2].weiduList)
+
+
+# mindis = me.shortestDis(k.areaList[1],k.areaList[2])
+# print(mindis)
+
+
+# for i in range(len(k.matrix)):
+# 	for j in range(len(k.matrix)):
+# 		print(k.matrix[i][j])
+# 	print('\n')
 
 
 
 
 
-# me = Measure()
-# temp = me.haversine(88.09763609415891,10.9655101537407369,88.09763609415891,10.9755101537407369)
+
+# temp = me.haversine(k.areaList[1].jingduList[0],k.areaList[1].weiduList[0],k.areaList[2].jingduList[0],k.areaList[2].weiduList[0])
 # print(temp)
 
 
