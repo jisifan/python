2b229607f7b5baab699298a7e21499ddb086d006
jisifan <503623869@qq.com>
2018-03-03 18:00:34 +0800

    initail matrix done

--- 地图相关/map.py
+++ 地图相关/map.py
@@ -2,6 +2,13 @@ import os
 from math import radians, cos, sin, asin, sqrt
 
 class LeastCover:
+	# def __init__(self,up,down,left,right):
+	# 	self.up = up
+	# 	self.down = down
+	# 	self.left = left
+	# 	self.right = right
+
+
 	def __init__(self,jingweidu):
 		self.up = max(jingweidu.weiduList)
 		self.down = min(jingweidu.weiduList)
@@ -9,7 +16,7 @@ class LeastCover:
 		self.right = max(jingweidu.jingduList)
 		
 	def toString(self):
-		print("up:"+ str(self.up) + "    down:" + str(self.down) + "    left:" + str(self.left) + "    right:" + str(self.right))
+		print("up:"+ str(self.up) + "	down:" + str(self.down) + "	left:" + str(self.left) + "	right:" + str(self.right))
 	
 
 
@@ -23,12 +30,60 @@ class JingWeiDu:
 		self.weiduList.append(weidu)
 
 
-class (object):
+class Measure:
 	"""经纬度的处理"""
-	def __init__(self, arg):
-		super(, self).__init__()
-		self.arg = arg
-		
+
+	def haversine(self,lon1, lat1, lon2, lat2): # 经度1，纬度1，经度2，纬度2 （十进制度数）  
+		""" 
+		Calculate the great circle distance between two points  
+		on the earth (specified in decimal degrees) 
+		"""  
+		# 将十进制度数转化为弧度  
+		lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+		# haversine公式  
+		dlon = lon2 - lon1   
+		dlat = lat2 - lat1   
+		a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2  
+		c = 2 * asin(sqrt(a))   
+		r = 6371 # 地球平均半径，单位为公里  
+		return c * r * 1000 
+
+	def isShortEnough(self,zoneA,zoneB):
+		""" 
+		两矩形的最短距离是否大致小于两公里，是返回true，否返回false
+		"""  
+
+		#纬度越高，经度单位差值越短
+		stdweidu = max(zoneA.up,zoneB.up)
+
+
+		#扩张区域A，直至上下左右都移出至少两公里
+		leftC = zoneA.left
+		while self.haversine(leftC,stdweidu,zoneA.left,stdweidu) < 2000:
+			leftC = leftC - 0.01
+
+		rightC = zoneA.right
+		while self.haversine(rightC,stdweidu,zoneA.right,stdweidu) < 2000:
+			rightC = rightC + 0.01
+
+
+		upC = zoneA.up
+		while self.haversine(zoneA.left,upC,zoneA.left,zoneA.up) < 2000:
+			upC = upC + 0.01
+
+		downC = zoneA.down
+		while self.haversine(zoneA.left,downC,zoneA.left,zoneA.down) < 2000:
+			downC = downC - 0.01
+
+
+		# A的扩展矩形和B是否相交，如果相交返回true
+		if downC > zoneB.up or leftC > zoneB.right or zoneB.down > upC or zoneB.left > rightC:
+			return False
+		else:
+			return True
+
+
 
 
 class FileHandle(object):
@@ -41,6 +96,7 @@ class FileHandle(object):
 			self.files = files
 			# 2表示未定义，1表示相邻，0表示不相邻
 			self.matrix = [[3 for i in range(len(files))] for i in range(len(files))]
+
 
 	# 处理kml文件，获取所有经纬度对
 	def kmlReader(self,filename):
@@ -70,19 +126,40 @@ class FileHandle(object):
 
 	# 获取所有县的上下左右边界
 	def getBoundList(self):
-		boundList = list()
+		self.boundList = list()
 		for file in self.files:
 			filename = self.root + '\\' + file
 			tt = self.kmlReader(filename)
-			boundList.append(tt[1])
-		for item in boundList:
-			item.toString()
+			self.boundList.append(tt[1])
+		# for item in boundList:
+		# 	item.toString()
 
-			
+
+	def initialMatrix(self):
+		me = Measure()
+		self.getBoundList()
+		for i in range(len(self.boundList)):
+			for j in range(i):
+				if not me.isShortEnough(self.boundList[i],self.boundList[j]):
+					self.matrix[i][j] = 0
+					self.matrix[j][i] = 0
+
 
 
 k = FileHandle('C:\maps')
-BoundList = k.getBoundList()
+k.initialMatrix()
+for i in range(len(k.matrix)):
+	for j in range(len(k.matrix)):
+		print(k.matrix[i][j])
+	print('\n')
+
+
+
+
+
+# me = Measure()
+# temp = me.haversine(88.09763609415891,10.9655101537407369,88.09763609415891,10.9755101537407369)
+# print(temp)
 
 
 
